99d537a0802129d757c1bf1a1355476e0a42fc9b
imtheskar <imtheskar@gmail.com>
2019-11-25 19:34:47 +0100

    Cambio a cliente 2

--- Proyecto_Tienda/src/POJOS/Cliente.java
+++ Proyecto_Tienda/src/POJOS/Cliente.java
@@ -22,5 +22,95 @@ public class Cliente {
     private Cuenta usuario;
     private String telefono;
     private Tarjeta tarjeta;
+
+    public Cliente() {
+    }
+
+    
+    public Cliente(String nombre, String apellidos, String dni, String correo_electronico, Direccion direccion, Date fecha_nacimiento, Cuenta usuario, String telefono, Tarjeta tarjeta) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.correo_electronico = correo_electronico;
+        this.direccion = direccion;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.usuario = usuario;
+        this.telefono = telefono;
+        this.tarjeta = tarjeta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public Cuenta getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Cuenta usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Tarjeta getTarjeta() {
+        return tarjeta;
+    }
+
+    public void setTarjeta(Tarjeta tarjeta) {
+        this.tarjeta = tarjeta;
+    }
+    
+    
     
 }
