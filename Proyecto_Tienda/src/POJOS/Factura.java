20fc84228db4ccb27a04fcf44fe58a849d76a87f
a18diegoirs <a18diegoirs@EQUIP077238.xunta.local>
2019-12-02 13:59:35 +0100

    Modificar Factura

--- Proyecto_Tienda/src/POJOS/Factura.java
+++ Proyecto_Tienda/src/POJOS/Factura.java
@@ -1,11 +1,28 @@
 package POJOS;
 
 import java.io.Serializable;
-
+import java.util.List;
+import javax.persistence.*;
 /**
  *
  * @author Usuario
  */
+
+
+@Entity
+@Table (name = "Factura")
+
 public class Factura implements Serializable{
+    
+    
+    @Id @GeneratedValue
+    @Column (name = "idFactura")
+    private int idFactura; 
+    
+    @Column (name = "precio")
+    private float precio; 
+    
+    @OneToMany (mappedBy = "Factura")
+    private List<Producto> productos;
     
 }
