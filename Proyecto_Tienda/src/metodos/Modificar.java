11f395d0b08b96faca7e8065edb31336468fdead
Diego <Diego@192.168.1.140>
2019-12-15 18:57:59 +0100

    Modificar terminado?

--- Proyecto_Tienda/src/metodos/Modificar.java
+++ Proyecto_Tienda/src/metodos/Modificar.java
@@ -57,14 +57,22 @@ public class Modificar {
         updateCuenta(cuenta);
     }
     
-    public static void dirección(Cliente cliente) throws IOException{
+    public static void dirección(Cuenta cuenta) throws IOException{
         System.out.println("Itroduce nueva direccion: ");
         String direccion = lee.readLine();
-        cliente.setDireccion(direccion);
+        cuenta.getCliente().setDireccion(direccion);
+        updateCuenta(cuenta);
     }
     
-    public static void usuario(Cliente cliente) throws IOException{
+    public static void usuario(Cuenta cuenta) throws IOException{
+        System.out.println("Introduce nuevo email: ");
+        String email = lee.readLine();
+        System.out.println("Introduce nueva contraseña: ");
+        String contraseña = lee.readLine();
         
+        cuenta.getCliente().setCorreo_electronico(email);
+        cuenta.setCorreo_electronico(email);
+        cuenta.setContraseña(contraseña);
     }
     
     public static void telefono(Cuenta cuenta) throws IOException{
@@ -72,10 +80,6 @@ public class Modificar {
         String tlf = Validar.telefono();
         cuenta.getCliente().setTelefono(tlf);
         updateCuenta(cuenta);
-    }
-    
-    public static void tarjeta(Cliente cliente) throws IOException{
-        
     }
     
     public static void updateCuenta(Cuenta cuenta) {
